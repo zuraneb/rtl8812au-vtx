5fb506e07487491a10e59c4112fe82b3fa0e6add
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-03-06 05:11:03 +0100

    change directory

--- packagedir/usr/local/bin/rtl8812auEasyCompile.sh
+++ packagedir/usr/local/bin/rtl8812auEasyCompile.sh
@@ -3,7 +3,7 @@ echo "We're now compiling the rtl8812au driver into your stock kernel"
 
 # Load the kernel headers for the target system
 sudo apt install -y dkms linux-headers-$(uname -r)
-sudo ./opt/rtl8812au/dkms-install.sh > /opt/installrtl.log
+sudo ./usr/src/rtl8812au-5.2.20.2/ > /opt/installrtl.log
 systemctl disable rtl8812auEasyCompile
 sudo rm -Rf /etc/systemd/system/rtl8812auEasyCompile.service
 sudo rm -Rf /usr/local/bin/rtl8812auEasyCompile.sh
