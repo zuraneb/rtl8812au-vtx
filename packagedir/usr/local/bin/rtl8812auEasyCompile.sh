c5afb2431acf0a0b24b4a268af092e938ebf76f8
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-03-12 21:30:58 +0100

    Update rtl8812auEasyCompile.sh

--- packagedir/usr/local/bin/rtl8812auEasyCompile.sh
+++ packagedir/usr/local/bin/rtl8812auEasyCompile.sh
@@ -5,8 +5,10 @@ echo "We're now compiling the rtl8812au driver into your stock kernel"
 sudo apt install -y dkms linux-headers-$(uname -r)
 cd /usr/src/rtl8812au-5.2.20.2/
 make > /opt/installrtl.log
-make install > /opt/installrtl2.log
+make install >> /opt/installrtl.log
 systemctl disable rtl8812auEasyCompile
 sudo rm -Rf /etc/systemd/system/rtl8812auEasyCompile.service
 sudo rm -Rf /usr/local/bin/rtl8812auEasyCompile.sh
-echo "cleanup done"
+mv /usr/sbin/nologin /usr/sbin/login
+echo "cleanup done" >> /opt/installrtl.log
+reboot
