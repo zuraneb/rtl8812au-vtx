4eb7366c3437b8189ba8e72c6819f497561ffb21
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-08-26 22:45:13 +0200

    debug

--- os_dep/linux/ioctl_linux.c
+++ os_dep/linux/ioctl_linux.c
@@ -3253,9 +3253,9 @@ static int rtw_wx_get_nick(struct net_device *dev,
 	/* struct security_priv *psecuritypriv = &padapter->securitypriv; */
 
 	if (extra) {
-		wrqu->data.length = 16;
+		wrqu->data.length = 14;
 		wrqu->data.flags = 1;
-		_rtw_memcpy(extra, "rtl8812au_openhd", 16);
+		_rtw_memcpy(extra, "<WORM@REALTEK>", 14);
 	}
 
 	/* rtw_signal_process(pid, SIGUSR1); */ /* for test */
