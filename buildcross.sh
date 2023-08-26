d202358ae1d6798ccdd9c4ee6cbe54f9622cac5d
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-08-26 11:36:42 +0200

    Update buildcross.sh

--- buildcross.sh
+++ buildcross.sh
@@ -14,18 +14,19 @@ sed -i 's/CONFIG_PLATFORM_I386_PC = y/CONFIG_PLATFORM_I386_PC = n/g' Makefile
 sed -i 's/CONFIG_PLATFORM_ARM64_RPI = n/CONFIG_PLATFORM_ARM64_RPI = y/g' Makefile
 sed -i 's/u8 fixed_rate = MGN_1M, sgi = 0, bwidth = 0, ldpc = 0, stbc = 0;/u8 fixed_rate = MGN_1M, sgi = 0, bwidth = 0, ldpc = 0, stbc = 1;/' core/rtw_xmit.c
 
+#using fixed directories for simplification
+mkdir -p /opt/rtl8812au
+cp -r * /opt/rtl8812au/
+
 #installing crosscompiler
-mkdir crosscompiler
+mkdir -p /opt/crosscompiler
 cd crosscompiler
 wget -q --show-progress --progress=bar:force:noscroll http://releases.linaro.org/components/toolchain/binaries/7.3-2018.05/aarch64-linux-gnu/gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu.tar.xz
 tar xf gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu.tar.xz
 export ARCH=arm64
 PACKAGE_ARCH=arm64
-cd ..
-export CROSS_COMPILE=crosscompiler/gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
-
-
-
+export CROSS_COMPILE=/opt/crosscompiler/gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+cd /opt/rtl8812au/
 
 #prepping kernel
 git clone https://github.com/OpenHD/RK_Kernel kernel --depth=1
@@ -33,15 +34,11 @@ cd kernel
 make rockchip_linux_defconfig && make prepare
 cd ..
 
-#dirty-hack
-mkdir -p /home/runner/work/rtl8812au/rtl8812au/drivers/net/wireless/rtl8812au/
-cp -r * /home/runner/work/rtl8812au/rtl8812au/drivers/net/wireless/rtl8812au/
-
 #build driver
-mkdir package
+mkdir -p /opt/package
 export KERNEL_VERSION="5.10.110-99-rockchip"
 make KSRC=kernel -j $J_CORES M=$(pwd) modules || exit 1
 mkdir -p package/lib/modules/${KERNEL_VERSION}/kernel/drivers/net/wireless/realtek/rtl8812au
-install -p -m 644 88XXau_wfb.ko "package/lib/modules/${KERNEL_VERSION}/kernel/drivers/net/wireless/88XXau_wfb.ko"
+install -p -m 644 88XXau_wfb.ko "/opt/package/lib/modules/${KERNEL_VERSION}/kernel/drivers/net/wireless/88XXau_wfb.ko"
 
-fpm -a arm64 -s dir -t deb -n rtl8812au -v 2.5-$(date '+%m%d%H%M') -C ./packagedir/ -p rtl8812au.deb
+fpm -a arm64 -s dir -t deb -n rtl8812au -v 2.5-$(date '+%m%d%H%M') -C /opt/packagedir/ -p rtl8812au.deb
