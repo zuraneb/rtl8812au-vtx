4703434143b8ddd61ec02091e62f220ba0b6317f
Roman Buldygin <froooks@gmail.com>
2023-09-27 17:34:42 +0300

    init demo app

---
+++ libusb_demo.cpp
@@ -0,0 +1,12 @@
+﻿// libusb_demo.cpp : Defines the entry point for the application.
+//
+
+#include "libusb_demo.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
