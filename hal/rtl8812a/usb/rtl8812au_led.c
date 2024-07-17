e74bc6f64abce5ebfa9562be7c10f353be4924e8
modaljc <jacob.camarillO@modalai.com>
2024-07-17 10:26:46 -0700

    blink all LEDs together

--- hal/rtl8812a/usb/rtl8812au_led.c
+++ hal/rtl8812a/usb/rtl8812au_led.c
@@ -168,19 +168,19 @@ SwLedOff_8812AU(
 			break;
 
 		case LED_PIN_LED0:
-			// RTW_PRINT("%s:: LED_PIN_LED0",__FUNCTION__);
+			RTW_PRINT("%s:: LED_PIN_LED0",__FUNCTION__);
 
-			// if (pHalData->AntDivCfg == 0) {
-			// 	// RTW_PRINT("%s:: LED_PIN_LED0::pHalData->AntDivCfg == 0",__FUNCTION__);
-			// 	LedCfg = rtw_read8(padapter, REG_LEDCFG0);
-			// 	LedCfg &= 0x70; /* Set to software control. */
-			// 	rtw_write8(padapter, REG_LEDCFG0, (LedCfg | BIT3 | BIT5));
-			// } else {
-			// 	RTW_PRINT("%s:: LED_PIN_LED0::pHalData->AntDivCfg != 0",__FUNCTION__);
-			// 	LedCfg = rtw_read8(padapter, REG_LEDCFG2);
-			// 	LedCfg &= 0xe0; /* Set to software control.			 */
-			// 	rtw_write8(padapter, REG_LEDCFG2, (LedCfg | BIT3 | BIT7 | BIT6 | BIT5));
-			// }
+			if (pHalData->AntDivCfg == 0) {
+				// RTW_PRINT("%s:: LED_PIN_LED0::pHalData->AntDivCfg == 0",__FUNCTION__);
+				LedCfg = rtw_read8(padapter, REG_LEDCFG0);
+				LedCfg &= 0x70; /* Set to software control. */
+				rtw_write8(padapter, REG_LEDCFG0, (LedCfg | BIT3 | BIT5));
+			} else {
+				RTW_PRINT("%s:: LED_PIN_LED0::pHalData->AntDivCfg != 0",__FUNCTION__);
+				LedCfg = rtw_read8(padapter, REG_LEDCFG2);
+				LedCfg &= 0xe0; /* Set to software control.			 */
+				rtw_write8(padapter, REG_LEDCFG2, (LedCfg | BIT3 | BIT7 | BIT6 | BIT5));
+			}
 			// break;
 
 		case LED_PIN_LED1:
@@ -188,7 +188,7 @@ SwLedOff_8812AU(
 			LedCfg = rtw_read8(padapter, REG_LEDCFG1);
 			LedCfg &= 0x70; /* Set to software control. */
 			rtw_write8(padapter, REG_LEDCFG1, (LedCfg | BIT3 | BIT5));
-			break;
+			// break;
 
 		case LED_PIN_LED2:
 			RTW_PRINT("%s:: LED_PIN_LED2",__FUNCTION__);
